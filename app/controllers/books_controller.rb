9f81876869b5f7dc7032ea0c65d941943ae9b1ed
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-16 17:34:54 +0900

    users, booksコントローラーを作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/create'
+  get 'books/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
