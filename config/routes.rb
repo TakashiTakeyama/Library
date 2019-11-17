f3a46fe27fface57a6b93893f08a0014b9ead6a9
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-17 18:40:40 +0900

    トップページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/create'
-  get 'books/update'
+  root 'homes#index'
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
